fa84133f388d900741939f9be8490d2cca4b4f95
Agidanicr7 <89138734+Agidanicr7@users.noreply.github.com>
2022-04-05 16:10:50 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,63 +4,25 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+let pRemark =document.getElementById('positive-alert')
+let nRemark = document.getElementById('negative-alert')
+let closeAlert = document.getElementsByClassName('closebtn')
 
-	var	$window = $(window),
-		$body = $('body');
+const showAlerts = () =>{
+	
+	pRemark.style.display = 'block'
+	nRemark.style.display = 'none'
+	
+	
+}
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+const showSadness = () => {
+	nRemark.style.display = 'block'
+	pRemark.style.display = 'none'
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+}
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			alignment: 'center',
-			detach: false
-		});
-
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo h1').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-})(jQuery);
+const Aclose = () => {
+	nRemark.style.display = 'none'
+	pRemark.style.display = 'none'
+}
